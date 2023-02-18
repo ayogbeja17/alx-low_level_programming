70f7beb9a6272f4bd2496189861ae430f6a48402
Ayodele Olorungbeja <ayudek862002@gmail.com>
2023-02-18 08:08:37 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*return: always 0 (success)
-int main(0)
+* main - entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
