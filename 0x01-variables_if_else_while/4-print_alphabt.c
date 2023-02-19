88b98c7d7260c80958e48c1d60b499b3493fee28
Ayodele Olorungbeja <ayudek862002@gmail.com>
2023-02-19 14:16:53 -0800

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Do not print e and q
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	if (lower != e && lower != q)
+		putchar(lower);
+			putchar('\n');
+	return (0);
+}
