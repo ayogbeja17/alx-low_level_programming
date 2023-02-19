ca4e7376711bd2cdfb8c70a9ec1d7bead1015b7f
Ayodele Olorungbeja <ayudek862002@gmail.com>
2023-02-19 13:53:50 -0800

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,11 @@
 int main(void)
 {
 char lower;
-char upper
-{
+char upper;
 for (lower = 'a'; lower <= 'z'; lower++)
 putchar(lower);
-}
-{
 for (upper = 'A'; upper <= 'Z'; upper++)
 putchar(upper);
-}
 putchar('\n');
 return (0);
 }
