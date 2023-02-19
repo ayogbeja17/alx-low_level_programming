7dc77026db87e261223298730608ec4eb5f60d02
Ayodele Olorungbeja <ayudek862002@gmail.com>
2023-02-19 14:19:47 -0800

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,13 @@
 */
 int main(void)
 {
-	char lower;
+char lower;
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	if (lower != e && lower != q)
-		putchar(lower);
-			putchar('\n');
-	return (0);
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+if (lower != e && lower != q)
+putchar(lower);
+}
+putchar('\n');
+return (0);
 }
