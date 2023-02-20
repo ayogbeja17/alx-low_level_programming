55eab9a841cc2688f03817e807c909805b037933
Ayodele Olorungbeja <ayudek862002@gmail.com>
2023-02-19 16:30:53 -0800

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * main - printing single digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
